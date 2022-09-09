7dc3d9f3d0571b750506d9032b1b5db973c0b028
Bazil <king.bazil.khan@gmail.com>
2022-09-09 01:59:04 +0000

    Updates html

--- festify/src/App.js
+++ festify/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav><p>this is where the nav bar will go FESTIFY EVENTS USER SEARCH PROFILE</p></nav>
+        <div className="header-box">
+                  {/* <img src={headerImg} className="header-photo" alt="header" /> */}
+                  <h2 className='greeting'>Welcome to Festify! Where you can experience your favourite festivals in your room or on your way to the real thing! Choose an event and Festify will generate a Spotify playlist based on the event's lineup!</h2>
+                </div>
       </header>
+      <body>
+        <div>
+                <div> 
+                  <p>this is where the events list will go</p>
+                </div>
+                {/* <EventList events={state.events} /> */}
+          </div>
+        </body>
     </div>
   );
 }
